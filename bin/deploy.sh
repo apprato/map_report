385872f98ad97bd2c031cdf8c3875af186cb2b16
Stephen Goudie <sgoudie@gmail.com>
2017-02-13 14:35:36 +1100

    Install script update:

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,17 +20,18 @@
 #CRON=emarsys.php
 
 installDeployLibraries () {
-  sudo apt-get update 
-  sudo apt-get install python-software-properties 
-  sudo apt-get install npm 
+  sudo apt-get update
+  sudo apt-get install python-software-properties
+  sudo apt-get install npm
   curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash
   nvm install 4.4.7
   nvm use 4.4.7
   sudo apt-get install awscli
   sudo npm install -g mupx
-  sudo npm install -g mupx-letsencrypt  
+  sudo npm install -g mupx-letsencrypt
   sudo npm install mup
   curl https://install.meteor.com | /bin/sh
+  sudo ln -s /usr/bin/nodejs /usr/bin/node
 }
 
 setupStaging() {
@@ -68,7 +69,7 @@ OPTIONS
 
 EXAMPLES
 sh bin/deploy.sh staging install installDeployLibraries
-sh bin/deploy.sh setupStaging    
+sh bin/deploy.sh setupStaging
 "
     >&2
     exit 1
--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-sudo apt-get update 
-sudo apt-get install python-software-properties 
-sudo apt-get install npm 
+sudo apt-get update
+sudo apt-get install python-software-properties
+sudo apt-get install npm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash
 nvm install 4.4.7
 nvm use 4.4.7
 sudo apt-get install awscli
 sudo npm install -g mupx
-sudo npm install -g mupx-letsencrypt  
+sudo npm install -g mupx-letsencrypt
+sudo npm install -g mup
 curl https://install.meteor.com | /bin/sh
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
